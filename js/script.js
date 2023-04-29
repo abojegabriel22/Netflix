9dbd8cae0a97fb9209d9b76fbff60593ba563bd7
aboje gabriel <abojejunior@gmail.com>
2023-04-29 19:32:21 +0100

    coupon

--- js/script.js
+++ js/script.js
@@ -4,3 +4,40 @@ function lightMode(){
     myLight.classList.toggle("myLight")
 
 }
+
+// COUPON LOAD FUNCTION 
+function loadCoupon(){
+    document.getElementById("coupon").style.visibility = "visible"
+    document.getElementById("header").style.opacity = "0.4"
+    document.getElementById("footer").style.opacity = "0.4"
+    document.getElementById("faq").style.opacity = "0.4"
+    document.getElementById("cartoon").style.opacity = "0.4"
+    document.getElementById("left").style.opacity = "0.4"
+    document.getElementById("tv1").style.opacity = "0.4"
+    document.getElementById("tv").style.opacity = "0.4"
+    document.getElementById("home").style.opacity = "0.4"
+}
+
+// COUPON CLOSE BUTTON 
+function closeCoupon(){
+    document.getElementById("coupon").style.visibility = "hidden"
+    document.getElementById("header").style.opacity = "1"
+    document.getElementById("footer").style.opacity = "1"
+    document.getElementById("faq").style.opacity = "1"
+    document.getElementById("cartoon").style.opacity = "1"
+    document.getElementById("left").style.opacity = "1"
+    document.getElementById("tv1").style.opacity = "1"
+    document.getElementById("tv").style.opacity = "1"
+    document.getElementById("home").style.opacity = "1"
+}
+function couponLink(){
+    document.getElementById("coupon").style.visibility = "hidden"
+    document.getElementById("header").style.opacity = "1"
+    document.getElementById("footer").style.opacity = "1"
+    document.getElementById("faq").style.opacity = "1"
+    document.getElementById("cartoon").style.opacity = "1"
+    document.getElementById("left").style.opacity = "1"
+    document.getElementById("tv1").style.opacity = "1"
+    document.getElementById("tv").style.opacity = "1"
+    document.getElementById("home").style.opacity = "1"
+}
